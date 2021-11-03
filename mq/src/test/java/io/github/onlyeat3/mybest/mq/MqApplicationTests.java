2975b432ddd2ab53bc9a091ec4e0bce42805e84a
onlyeat3 <onlyeat3@gmail.com>
2021-11-03 16:07:50 +0800

    add mq demo project

---
+++ mq/src/test/java/io/github/onlyeat3/mybest/mq/MqApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.onlyeat3.mybest.mq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
